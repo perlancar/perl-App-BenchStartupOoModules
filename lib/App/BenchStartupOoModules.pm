ef05ead29af82f035f950e036350156c41497d82
perlancar (@pc-home) <perlancar@gmail.com>
2015-06-14 12:14:49 +0700

    Initial commit

---
+++ lib/App/BenchStartupOoModules.pm
@@ -0,0 +1,11 @@
+package App::BenchStartupOoModules;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: Benchmark startup time of various OO system modules
+
+=head1 SYNOPSIS
+
+See the included script L<bench-startup-oo-modules>.
